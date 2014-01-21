a48cc5b1644fa43639d373a30c6889d35501daf2
Allen Rohner <arohner@gmail.com>
2014-01-20 16:09:01 -0800

    Bump version, deps

--- project.clj
+++ project.clj
@@ -1,11 +1,9 @@
-(defproject circleci/version-specs "0.1.0"
+(defproject circleci/version-specs "0.1.1"
   :description "plugin fns for lein-version-specs"
-  :version-spec "0.1.~{:env/circle_build_num}"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.eclipse.jgit/org.eclipse.jgit "2.0.0.201206130900-r"]]
-  :repositories [["circle-jars" {:url "s3p://circle-jars/releases"}]]
-  :plugins [[lein-version-spec "0.0.2"]]
+  :plugins [[lein-version-spec "0.0.3"]]
   :profiles {:dev
              {:dependencies
               [[org.clojure/clojure "1.5.1"]]}})
